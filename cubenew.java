c87a2e8687fbe406eeda1a4d20eeff19eddda3c8
rishikesh1797 <101618071+rishikesh1797@users.noreply.github.com>
2022-03-15 12:56:16 +0530

    Add files via upload

---
+++ cubenew.java
@@ -0,0 +1,11 @@
+class Fun{    
+       static void cube(int n1){
+        int result = n1*n1*n1;
+        System.out.println(result);
+       }
+        public static void main(String[] args)
+       {
+        cube(9);
+        }
+        }
+        
